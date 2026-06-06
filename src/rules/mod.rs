f075545c6efd7264c12650d7adddf52a89d51005
ahan-halder <halderahan.2005@gmail.com>
2026-06-06 14:55:32 +0530

    Modified rules

--- src/rules/mod.rs
+++ src/rules/mod.rs
@@ -12,11 +12,19 @@ pub struct RuleSet {
 
 impl RuleSet {
     pub fn load_from_path(path: impl AsRef<Path>) -> Result<Self, String> {
-        let contents = fs::read_to_string(path.as_ref())
-            .map_err(|error| format!("failed to read rules file {}: {error}", path.as_ref().display()))?;
+        let contents = fs::read_to_string(path.as_ref()).map_err(|error| {
+            format!(
+                "failed to read rules file {}: {error}",
+                path.as_ref().display()
+            )
+        })?;
 
-        let file: schema::RuleFile = serde_yaml::from_str(&contents)
-            .map_err(|error| format!("failed to parse YAML rules in {}: {error}", path.as_ref().display()))?;
+        let file: schema::RuleFile = serde_yaml::from_str(&contents).map_err(|error| {
+            format!(
+                "failed to parse YAML rules in {}: {error}",
+                path.as_ref().display()
+            )
+        })?;
 
         Ok(Self { rules: file.rules })
     }
